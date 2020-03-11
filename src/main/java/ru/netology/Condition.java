c478ec4539d679df892b06f752539a3760e5bd12
mkovarky <mikhail.kovarsky@gmail.com>
2020-03-11 20:42:03 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Condition.java
@@ -0,0 +1,76 @@
+package ru.netology;
+
+public class Condition {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+/*        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
+*/
+        this.currentTemperature = currentTemperature;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+
+    public void increaseCurrentTemperature() {
+        if (currentTemperature >= maxTemperature) {
+            return;
+        }
+        while (currentTemperature < maxTemperature) {
+            currentTemperature = currentTemperature + 1;
+        }
+
+    }
+
+    public void decreaseCurrentTemperature() {
+        if (currentTemperature <= minTemperature) {
+            return;
+        }
+        while (currentTemperature > minTemperature) {
+            currentTemperature = currentTemperature - 1;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/ConditionTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionTest {
+
+    @Test
+    void increaseCurrentTemperatureFromNormalValue() {
+        Condition condition = new Condition();
+        condition.setCurrentTemperature(20);
+        condition.setMaxTemperature(30);
+        condition.increaseCurrentTemperature();
+        assertEquals(condition.getMaxTemperature(),condition.getCurrentTemperature());
+    }
+
+    @Test
+    void increaseCurrentTemperatureFromMaxValue() {
+        Condition condition = new Condition();
+        condition.setCurrentTemperature(30);
+        condition.setMaxTemperature(30);
+        condition.increaseCurrentTemperature();
+        assertEquals(condition.getMaxTemperature(),condition.getCurrentTemperature());
+    }
+
+    @Test
+    void decreaseCurrentTemperatureFromNormalValue() {
+        Condition condition = new Condition();
+        condition.setCurrentTemperature(20);
+        condition.setMinTemperature(16);
+        condition.decreaseCurrentTemperature();
+        assertEquals(condition.getMinTemperature(),condition.getCurrentTemperature());
+    }
+
+    @Test
+    void decreaseCurrentTemperatureFromMinValue() {
+        Condition condition = new Condition();
+        condition.setCurrentTemperature(16);
+        condition.setMinTemperature(16);
+        condition.decreaseCurrentTemperature();
+        assertEquals(condition.getMinTemperature(),condition.getCurrentTemperature());
+    }
+}
